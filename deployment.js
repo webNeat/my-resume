f75f58d30fff16a04663066cdd73cfcf9a7a252f
Amine Ben hammou <webneat@gmail.com>
2024-10-14 01:17:04 +0200

    Updgrade CI and dependencies

--- deployment.js
+++ deployment.js
@@ -1,20 +1,18 @@
+import yaml from 'yaml'
 import { ci, deploy, app, destroy } from 'hosty'
 
 const repo = ci.repo()
 const branch = ci.branch()
 const domain = (branch !== 'main' ? branch + '.' : '') + 'webneat.me'
-const env = {
-  TZ: 'UTC',
-  PORT: '80',
-  HOST: '0.0.0.0',
-  LOG_LEVEL: 'info',
-  APP_KEY: 'NZWllL-iR0n8OaQQyQzH6YWwojZNWOYH',
-  NODE_ENV: 'production',
-  SESSION_DRIVER: 'cookie',
-}
-const application = app.git({ name: domain, domain, repo, branch, env })
+const vars = ci.vars()
 
-if (ci.event() === 'delete') destroy(ci.server(), application)
-else deploy(ci.server(), application)
+console.log('vars', vars);
 
-ci.run()
+
+// const env = yaml.parse(vars.prod_env)
+// const application = app.git({ name: domain, domain, repo, branch, env })
+
+// if (ci.event() === 'delete') destroy(ci.server(), application)
+// else deploy(ci.server(), application)
+
+// ci.run()
